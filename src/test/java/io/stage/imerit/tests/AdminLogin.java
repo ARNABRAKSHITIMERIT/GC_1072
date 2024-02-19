87bf2652bb602a8ed93c8dd0f7dc391ee52db73e
ARNABRAKSHITIMERIT <124165614+ARNABRAKSHITIMERIT@users.noreply.github.com>
2024-02-19 14:31:28 +0530

    Update AdminLogin.java

--- src/test/java/io/stage/imerit/tests/AdminLogin.java
+++ src/test/java/io/stage/imerit/tests/AdminLogin.java
@@ -55,7 +55,7 @@ public class AdminLogin extends BaseClass {
 
 		loginPage.clickOnEmailPasswordNext();
 
-		thread.sleep(10000) ;
+		Thread.sleep(10000) ;
 		
 		//WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(30));
 		//wait2.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[@class=\"XY0ASe\"]")));
